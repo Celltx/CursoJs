0a398f50ef4532d0c3194ae284d9954a8c500f4c
Celltx <lluvianava70@gmail.com>
2026-01-28 14:03:53 -0600

    First Commit

---
+++ codigo.js
@@ -0,0 +1,28 @@
+//Tipos de datos 
+console.log("Tipos de datos");
+//Numeros
+entero = 1;
+decimal = 1.1;
+negativo = -1;
+console.log(`Entero:  ${entero}, Decimal: ${decimal}, Negativo: ${negativo}`);
+
+//Strings
+nombre = "Celeste"
+console.log(`String: ${nombre}`);
+
+//Booleans
+verdadero = true;
+falso = false;
+console.log(`Boolean: Verdadero: ${verdadero}, Falso: ${falso}`);
+
+//Null
+variableVacia = null;
+console.log(variableVacia);
+
+//Naan
+nan = 1 * "a";
+console.log(nan)
+
+//Undefrined 
+let indefinido;
+console.log(indefinido);
