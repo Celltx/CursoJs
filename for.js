7935b2034d86bd793f7e07c5d870caeddcc9eaf7
Celltx <lluvianava70@gmail.com>
2026-02-04 16:00:39 -0600

    Ciclo for

---
+++ for.js
@@ -0,0 +1,23 @@
+/*
+Contador Inverso: Crea un programa que cuente del 10 al 1 y lo imprima en la consola.
+
+*/
+
+for(let i=10; i>0; i--){
+    console.log(i)
+}
+
+
+/*
+Suma de Pares: Modifica el ciclo para que solo sume los números pares del 1 al 20 y muestre el resultado
+*/
+ let suma = 0;
+
+for(let i=0; i<20; i++){
+    if (i%2 == 0){
+    suma=+ i;
+       console.log(i);
+    }
+ 
+}
+
