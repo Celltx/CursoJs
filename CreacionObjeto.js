5b26068ecc063bc7304192c725a8f7fe2fa586f0
Celltx <lluvianava70@gmail.com>
2026-03-04 09:21:01 -0600

    Objeto libro

---
+++ CreacionObjeto.js
@@ -0,0 +1,32 @@
+// Instrucciones para resolver el problema:
+// Cada libro debe ser un objeto con las siguientes propiedades: titulo: (string) el título del libro, autor: (string) el autor del libro, anio: (number) el año de publicación, estado: (string) el estado del libro, que puede ser 'disponible' o 'prestado'.
+// También debe tener un método describirLibro: (method) método para imprimir la información básica del libro. Algo como 'Libro titulado [titulo], escrito por [autor] en el año [anio], el estado es: [estado].'
+// Opcional: agregar una propiedad que contenga la lista de capítulos del libro y métodos que permitan agregar y eliminar capítulos del libro.
+
+class libros {
+    constructor (titulo, autor, anio, estado){
+        this.titulo=titulo;
+        this.autor=autor;
+        this.anio=anio;
+        this.estado=estado;
+        this.capitulos=[];
+    }
+    describirLibro(){
+        console.log(`Libro titulado ${this.titulo}, escrito por ${this.autor}, en el año ${this.anio}, el estado es ${this.estado}.`)
+    }
+    agregarLibro(capitulos){
+        this.capitulos.push(capitulos);
+    }
+    eliminarCapitulo(){
+        this.capitulos.pop(this.capitulos);
+    }
+
+}
+const libro1 = new libros(
+    "El juego de la lógica",
+    "Lewis Carroll",
+    1887,
+    "prestado"
+);
+
+libro1.describirLibro();
